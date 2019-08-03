3e8bbdd753cdea9ceecbeccfc2009b4e7708b7e2
jung-han <hanjung@nhnent.com>
2019-08-03 22:13:19 +0900

    feat: fetch example

--- src/components/splashScreen.js
+++ src/components/splashScreen.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Button, ActivityIndicator } from 'react-native';
+import * as url from '../apiUrl';
 
 export default function RegisterName(props) {
+  const [test, setTest] = useState(null);
   const { navigation } = props;
+  useEffect(() => {
+    fetch(url.test(), { method: 'GET' })
+      .then((res) => res.json())
+      .then((resJson) => {
+        setTest(resJson.result);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  });
 
   return (
     <View style={styles.splash}>
-      <Text>splashScreen</Text>
+      <Text>HELLO</Text>
+      {test ? (
+        test.map((elem) => {
+          return (
+            <Text key={elem.name}>
+              {elem.age}, {elem.name}
+            </Text>
+          );
+        })
+      ) : (
+        <ActivityIndicator size="small" color="#0099ED" />
+      )}
       <Button title="go registerName" onPress={() => navigation.navigate('RegisterName')} />
     </View>
   );
@@ -16,6 +39,6 @@ const styles = StyleSheet.create({
   splash: {
     flex: 1,
     justifyContent: 'center',
-    alignContent: 'center'
+    alignItems: 'center'
   }
 });
