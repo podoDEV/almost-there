a9c5463bc455b8b9809659b009b08fb10bd1fc35
jung-han <hanjung@nhnent.com>
2019-08-11 15:05:41 +0900

    feat: add global context

--- src/components/registerName.js
+++ src/components/registerName.js
@@ -1,17 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Layout } from '../layout';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, AsyncStorage } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { GlobalContext } from '../context';
 
 export default function RegisterName(props) {
   const { navigation } = props;
   const [name, setName] = useState('');
+  const userInfo = useContext(GlobalContext);
+
+  useEffect(() => {
+    getUserSession();
+  }, []);
+
+  async function getUserSession() {
+    const sessionId = await AsyncStorage.getItem('SESSION_ID');
+    if (sessionId) {
+      userInfo.sessionId = sessionId;
+      navigation.navigate('GroupMap');
+    }
+  }
+
+  async function handlePressIcon() {
+    try {
+      await AsyncStorage.setItem('SESSION_ID', '1234');
+      await navigation.navigate('GroupMap');
+    } catch (err) {
+      // @TODO: 에러 팝업!
+      console.error(err);
+    }
+  }
 
   return (
     <Layout>
       <View style={styles.container}>
         <Text style={styles.title}>그냥 이름만</Text>
-        <TextInput style={styles.nameInput} onChangeText={(text) => setName(text)} value={name} />
-        <Button color="white" title="완료👉" onPress={() => navigation.navigate('GroupMap')} />
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.nameInput} onChangeText={(text) => setName(text)} value={name} />
+          <MaterialCommunityIcons
+            style={styles.icon}
+            name="arrow-right"
+            size={20}
+            color="#fff"
+            onPress={handlePressIcon}
+          />
+        </View>
       </View>
     </Layout>
   );
@@ -30,15 +63,25 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 29
   },
-  nameInput: {
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     borderBottomWidth: 1,
     borderColor: '#fff',
+    marginTop: 10,
+    marginLeft: 23,
+    marginRight: 23
+  },
+  nameInput: {
+    flex: 9,
     color: '#fff',
     fontSize: 25,
-    padding: 10,
-    marginLeft: 23,
-    marginRight: 23,
     textAlign: 'center',
-    marginBottom: 15
+    marginBottom: 5,
+    paddingLeft: 25
+  },
+  icon: {
+    flex: 1
   }
 });
