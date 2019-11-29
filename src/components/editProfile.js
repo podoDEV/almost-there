a84a125f40c90c32f43be5cd1ca4cd9c33f5e3dc
jung-han <hanjung@nhnent.com>
2019-11-29 20:50:56 +0900

    feat: edit profile markup, map update timing

--- src/components/editProfile.js
+++ src/components/editProfile.js
@@ -1,5 +1,6 @@
-import React, { useState, useContext } from 'react';
-import { View, Image, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, TouchableOpacity, Text, StyleSheet, ImageBackground, TextInput } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import Constants from 'expo-constants';
 import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
@@ -29,13 +30,89 @@ export default function editProfile(props) {
   }
 
   return (
-    <View>
-      <TouchableOpacity onPress={pickImage}>
-        <Image source={{ uri: profileImageUrl }} style={{ width: 100, height: 100 }} />
-        <Text>{name}</Text>
+    <View style={styles.container}>
+      <View style={styles.infoContainer}>
+        <View style={styles.photoButtonContainer}>
+          <TouchableOpacity style={styles.imageUploadButton} onPress={pickImage}>
+            <ImageBackground source={{ uri: profileImageUrl }} style={styles.image}>
+              <View
+                style={{
+                  flex: 1,
+                  backgroundColor: 'rgba(255, 255, 255, 0.4)',
+                  justifyContent: 'center',
+                  alignItems: 'center'
+                }}
+              >
+                <MaterialCommunityIcons
+                  name="square-edit-outline"
+                  size={25}
+                  color="rgb(74,74,74)"
+                />
+              </View>
+            </ImageBackground>
+          </TouchableOpacity>
+        </View>
+        <TextInput style={styles.nameInput} onChangeText={(text) => setName(text)} value={name} />
+      </View>
+      <TouchableOpacity
+        style={styles.finishBtnContainer}
+        onPress={() => {
+          alert('수정!');
+        }}
+      >
+        <Text style={styles.finishBtn}>수정 완료</Text>
       </TouchableOpacity>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  infoContainer: {
+    flex: 8,
+    justifyContent: 'center'
+  },
+  finishBtnContainer: {
+    flex: 2,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  finishBtn: {
+    fontFamily: 'scdreamBold',
+    color: '#0099ED',
+    fontSize: 21
+  },
+  photoButtonContainer: {
+    alignItems: 'center'
+  },
+  image: {
+    flex: 1,
+    width: null,
+    height: null
+  },
+  imageUploadButton: {
+    justifyContent: 'center',
+    alignContent: 'center',
+    height: 130,
+    width: 130,
+    borderColor: 'rgb(74,74,74)',
+    borderWidth: 1.5,
+    borderRadius: 100,
+    marginTop: 17,
+    overflow: 'hidden'
+  },
+  nameInput: {
+    fontFamily: 'scdream',
+    color: '#000',
+    fontSize: 19,
+    textAlign: 'center',
+    padding: 8,
+    borderColor: '#fff',
+    borderBottomColor: 'rgb(213,213,213)',
+    borderWidth: 1,
+    marginTop: 35,
+    marginHorizontal: 25
+  }
+});
